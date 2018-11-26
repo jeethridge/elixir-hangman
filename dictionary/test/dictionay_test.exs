80c584e67bb75bee978d9487456b666f46f572c3
James Ethridge <jeethridge@gmail.com>
2018-11-25 21:10:42 -0500

    adds test to ensure worslist is accessable

--- dictionary/test/dictionay_test.exs
+++ dictionary/test/dictionay_test.exs
@@ -2,7 +2,7 @@ defmodule DictionayTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "greets the world" do
-    assert Dictionary.hello() == :world
+  test "can get a random word from the wordlist asset" do
+    assert is_binary(Dictionary.random_word())
   end
 end
