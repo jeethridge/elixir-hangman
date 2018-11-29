8868b9daea2d200214a47b5e989e2538c3633f96
James Ethridge <jeethridge@gmail.com>
2018-11-28 21:23:12 -0500

    adds a text_client package to the project for building a simple UI.

---
+++ text_client/lib/text_client/interact.ex
@@ -0,0 +1,23 @@
+defmodule TextClient.Interact do
+
+  alias TextClient.State
+
+  def start() do
+    Hangman.new_game()
+    |> setup_state()
+    |> IO.inspect
+  end
+
+  defp setup_state(game) do
+    %State{
+      game_service: game,
+      tally: Hangman.tally(game)
+    }
+  end
+
+  def play(state) do
+    #state = do_stuff(state)
+    play(state)
+  end
+
+end
