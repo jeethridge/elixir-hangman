b14347de60af55da9a823139910cf43c3be89b16
James Ethridge <jeethridge@gmail.com>
2018-12-17 21:38:10 -0500

    fixes typo in handle_call for tally

--- hangman/lib/hangman/server.ex
+++ hangman/lib/hangman/server.ex
@@ -17,7 +17,7 @@ defmodule Hangman.Server do
     {:reply, tally, game}
   end
 
-  def handl_call({ :tally }, _from, game) do 
+  def handle_call({ :tally }, _from, game) do 
     { :reply, Game.tally(game), game }
   end
 
