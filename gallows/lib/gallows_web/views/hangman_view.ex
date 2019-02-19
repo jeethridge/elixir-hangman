1b30c5d0000fc267b0d26e7e88cd904bb0622533
James Ethridge <jeethridge@gmail.com>
2019-02-18 21:12:25 -0500

    adds helpers for alerts and starting new games to improve ux

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,14 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  import Gallows.Views.Helpers.GameStateHelper
+
+  def new_game_button(conn) do
+    button("Enter the Gallows", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  def game_over?(%{game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
 end
