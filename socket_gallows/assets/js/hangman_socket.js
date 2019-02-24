ab483193d2bbaefe0aabc73a6d58334c0019fd2e
James Ethridge <jeethridge@gmail.com>
2019-02-23 19:47:31 -0500

    adds logic to create a channel which starts a game and returns a tally.

--- socket_gallows/assets/js/hangman_socket.js
+++ socket_gallows/assets/js/hangman_socket.js
@@ -3,9 +3,34 @@ import { Socket } from "phoenix";
 export default class HangmanSocket {
 
     constructor() {
+        console.log("Connecting to hangman.")
         this.socket = new Socket("/socket", {})
         this.socket.connect()
-        console.log("Connected to user socket.")
+    }
+
+    
+    connect_to_hangman() {
+        this.setup_channel()
+        this.channel.on("tally", tally =>
+            console.dir(tally)
+        )        
+    }
+
+    setup_channel() {
+        this.channel = this.socket.channel("hangman:game", {})
+        this.channel.join()
+        .receive("ok", resp => {
+            console.log("Connected to hangman channel.")
+            this.fetch_tally()
+        })
+        .receive("error", resp => {
+
+        })        
+    } 
+
+    fetch_tally() {
+        this.channel.push("tally", {})
+        this.channel.push("nonsense", {})
     }
 
 }
--- socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
@@ -1,8 +1,24 @@
 defmodule SocketGallowsWeb.HangmanChannel do
   use Phoenix.Channel
+  require Logger
 
   def join("hangman:game", _, socket) do
+    game = Hangman.new_game()
+    socket = assign(socket, :game, game)
     { :ok, socket }
+  end
+
+  def handle_in("tally", _, socket) do
+    game = socket.assigns.game
+    tally = Hangman.tally(game)
+    push(socket, "tally", tally)
+    {:noreply, socket}
+  end
+
+  # Default unknown event handler
+  def handle_in(request, _, socket) do
+    Logger.error("Unknown event: #{request} on #{socket.channel}")
+    {:noreply, socket}
   end
 
 end
