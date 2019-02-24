6152b4b59a269a9cfef48f67bc46f2e8ed3f7806
James Ethridge <jeethridge@gmail.com>
2019-02-23 23:45:54 -0500

    adds dave's vue.js frontend but styling is bad for me.

--- socket_gallows/assets/js/hangman_socket.js
+++ socket_gallows/assets/js/hangman_socket.js
@@ -1,36 +1,49 @@
-import { Socket } from "phoenix";
+import {Socket} from "phoenix"
 
 export default class HangmanSocket {
-
-    constructor() {
-        console.log("Connecting to hangman.")
-        this.socket = new Socket("/socket", {})
+    constructor(tally) {
+        this.tally  = tally
+        this.socket = new Socket("/socket", { })
         this.socket.connect()
     }
 
-    
     connect_to_hangman() {
-        this.setup_channel()
-        this.channel.on("tally", tally =>
-            console.dir(tally)
-        )        
+        this.setup_channel();
+        this.channel
+            .join()
+            .receive("ok", resp => {
+                console.log("Joined successfully", resp)
+                this.fetch_tally()
+            })
+            .receive("error", resp => {
+                alert("Unable to join", resp)
+                throw(resp)
+            })
     }
 
     setup_channel() {
         this.channel = this.socket.channel("hangman:game", {})
-        this.channel.join()
-        .receive("ok", resp => {
-            console.log("Connected to hangman channel.")
-            this.fetch_tally()
+        this.channel.on("tally", (tally) => {
+            this.copy_tally(tally)
         })
-        .receive("error", resp => {
-
-        })        
-    } 
+    }
 
     fetch_tally() {
         this.channel.push("tally", {})
-        this.channel.push("nonsense", {})
     }
 
+    make_move(guess) {
+        this.channel.push("make_move", guess)
+    }
+
+    new_game() {
+        this.channel.push("new_game", {})
+    }
+    
+    copy_tally(from) {
+        for (let k in from) {
+            this.tally[k] = from[k]
+        }
+    }
 }
+
--- socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
@@ -15,7 +15,18 @@ defmodule SocketGallowsWeb.HangmanChannel do
     {:noreply, socket}
   end
 
-  # Default unknown event handler
+  def handle_in("make_move", guess, socket) do
+    tally = socket.assigns.game |> Hangman.make_move(guess)
+    push(socket, "tally", tally)
+    { :noreply, socket }
+  end
+
+  def handle_in("new_game", _, socket) do
+    socket = socket |> assign(:game, Hangman.new_game())
+    handle_in("tally", nil, socket)
+  end
+
+  # Default handler
   def handle_in(request, _, socket) do
     Logger.error("Unknown event: #{request} on #{socket.channel}")
     {:noreply, socket}
